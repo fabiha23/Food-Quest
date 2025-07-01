629213ba90d637aa07dd367a28c9188f27492658
HP <fabiha.amatullah@gmail.com>
2025-07-01 17:03:42 +0600

    feat:setup with react router, tailwind

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+
+const RootLayout = () => {
+  return (
+    <div>
+      <header className="bg-base-100 sticky top-0 shadow-lg z-50">
+        <header className="max-w-7xl xl:mx-auto xl:px-2 lg:px-6 mx-3">
+          <Navbar></Navbar>
+        </header>
+      </header>
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </div>
+  );
+};
+
+export default RootLayout;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="bg-base-200 min-h-[calc(100vh-80px)]">
+        <div className="max-w-7xl xl:mx-auto xl:px-2 lg:px-6 mx-3">
+          home
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
